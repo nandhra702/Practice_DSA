3791674cde4d90a82c31d22edc0174459bdfd83d
nandhra702 <sukhrajsinghnandhra@gmail.com>
2025-06-20 23:49:57 +0530

    Will get easy with practice

---
+++ Binary_Tree/124.maximum-pathsum_HARD.cpp
@@ -0,0 +1,53 @@
+/*
+ FIRST POSITIVE MISSING NUMBER : 41
+TOPIC - ARRAYS
+LEVEL - HARD
+SOURCE - EMAIL
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        //first we need a different function to call for finding the sum
+        int maxi = INT_MIN;
+
+        maxPathDown(root,maxi);
+        return maxi;
+        
+    }
+
+    int maxPathDown(TreeNode* root, int& maxi){
+        if(root==NULL){
+            return 0;
+        }
+
+        //we are taking zero as we do not want negative numbers to be there
+
+        int left = max(0,maxPathDown(root->left,maxi));
+        int right = max(0,maxPathDown(root->right,maxi));
+
+        maxi = max(maxi, left + right + root->val);
+
+        return max(left,right) + root->val;
+
+
+    }
+
+
+
+
+};
+// @lc code=end
+
